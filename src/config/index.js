b541c1f75a91c893e840b504d6c15bb68dd5d8dd
Martina <10175022248@qq.com>
2024-02-21 23:35:13 +0800

    feat: 架构设计-环境配置

---
+++ src/config/index.js
@@ -0,0 +1,24 @@
+/**
+ * 环境配置封装
+ */
+const env = import.meta.env.MODE || 'prod';
+const EnvConfig = {
+  dev: {
+    baseApi: '/api',
+    mockApi: 'https://dev.usemock.com/65cf84576309cc7a37782edd'
+  },
+  test: {
+    baseApi: '//test.futurefe.com/api',
+    mockApi: 'https://dev.usemock.com/65cf84576309cc7a37782edd'
+  },
+  prod: {
+    baseApi: '//futurefe.com/api',
+    mockApi: 'https://dev.usemock.com/65cf84576309cc7a37782edd'
+  }
+}
+
+export default {
+  env,
+  mock: true,
+  ...EnvConfig[env]
+}
