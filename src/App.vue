536635ba00e0ce52321f72b3ea0fc06de0e1b8af
Martina <10175022248@qq.com>
2024-02-24 14:03:09 +0800

    feat: 首页布局开发

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,11 @@
 
 <script>
   export default {
-    name: 'app',
-    mounted(){
-      this.$storage.setItem("name", "amy");
-      this.$storage.setItem("age", 30);
-      this.$storage.setItem("age", 50);
-      console.log(this.$storage.getItem("name"));
-      this.$storage.setItem("name", "lucy");
-      console.log(this.$storage.getItem("name"));
-      this.$storage.clearItem("name")
-      this.$storage.setItem("userinfo", { name: "lili" })
-      // this.$storage.clearAll();
-    }
+    name: 'app'
   }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+@import './assets/style/reset.css';
+@import './assets/style/index.scss';
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,9 +5,57 @@ export default {
 </script>
 
 <template>
-  <h1>首页首页首页Home</h1>
-  <router-view></router-view>
+  <div class="basic-layout">
+    <div class="nav-side"></div>
+    <div class="content-right">
+      <div class="nav-top">
+        <div class="bread">面包屑</div>
+        <div class="user-info">用户</div>
+      </div>
+      <div class="wrapper">
+        <div class="main-page">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style lang="scss">
+.basic-layout {
+  position: relative;
+
+  .nav-side {
+    position: fixed;
+    width: 200px;
+    height: 100vh;
+    background-color: #001529;
+    color: #fff;
+    overflow-y: auto;
+    transition: width .5s;
+  }
+
+  .content-right {
+    margin-left: 200px;
+
+    .nav-top {
+      height: 50px;
+      line-height: 50px;
+      border-bottom: solid #ddd 1px;
+      display: flex;
+      justify-content: space-between;
+      padding: 0 20px;
+    }
+
+    .wrapper {
+      background: #eef0f3;
+      padding: 20px;
+      height: calc(100vh - 50px);
+      .main-page {
+        background: #fff;
+        height: 100%;
+      }
+    }
+  }
+
+}</style>
