ea459ba658b02966aba774d94c628ff9ca0ad8a3
Martina <10175022248@qq.com>
2024-03-05 18:22:20 +0800

    feat: 欢迎页面实现

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,20 +1,47 @@
-<script>
-import { useRouter } from 'vue-router';
-export default {
-  name: 'Welcome',
-  setup(){
-    const router = useRouter();
-    function goLogin() {
-      router.push('/login');
-    }
-    return  { goLogin }
-  }
-} 
-</script>
-
 <template>
-  <h2>这里是欢迎页面</h2>
-  <el-button @click="goLogin">去登陆</el-button>
+  <div class="welcome">
+    <div class="content">
+      <div class="sub-title">欢迎体验</div>
+      <div class="title">慕课通用后台管理系统</div>
+      <div class="desc">- Vue3.0+ElementPlus+Node+Mongo打造通用后台管理系统</div>
+    </div>
+    <div class="img"></div>
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+</script>
+
+<style scoped lang="scss">
+.welcome {
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  .content {
+    position: relative;
+    .sub-title {
+      font-size: 30px;
+      color: #333;
+      line-height: 42px;
+    }
+
+    .title {
+      font-size: 40px;
+      color: #409eff;
+      line-height: 62px;
+    }
+
+    .desc {
+      font-size: 14px;
+      color: #999;
+      text-align: right;
+    }
+  }
+  .img {
+    width: 371px;
+    height: 438px;
+    background-image: url('./../assets/images/welcome.png');
+  }
+}
+</style>
