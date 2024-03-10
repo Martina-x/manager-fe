e6dfc6c23f657f377d62b59b9553e93e177ea83f
Martina <10175022248@qq.com>
2024-03-10 15:41:10 +0800

    feat: 7-5用户新增实现

--- src/views/User.vue
+++ src/views/User.vue
@@ -24,7 +24,7 @@
     </div>
     <div class="base-table">
       <div class="action">
-        <el-button type="primary">新增</el-button>
+        <el-button type="primary" @click="handleCreate">新增</el-button>
         <el-button type="danger" @click="handlePatchDel">批量删除</el-button>
       </div>
       <el-table :data="userList" @selection-change="handleSelectionChange">
@@ -43,6 +43,62 @@
           :page-size="pager.pageSize" @current-change="handleCurrentChange" />
       </div>
     </div>
+    <el-dialog v-model="showModal" title="新增用户">
+      <el-form :model="dialogForm" label-width="100px" :rules="rules" ref="dialogFormRef">
+        <el-form-item prop="userName" label="用户名">
+          <el-input v-model="dialogForm.userName" />
+        </el-form-item>
+        <el-form-item label="邮箱" prop="userEmail">
+          <el-input v-model="dialogForm.userEmail" placeholder="请输入用户邮箱">
+            <template #append>.com</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="手机号" prop="mobile">
+          <el-input v-model="dialogForm.mobile" placeholder="请输入用户手机号" />
+        </el-form-item>
+        <el-form-item label="岗位" prop="job">
+          <el-input v-model="dialogForm.job" placeholder="请输入用户岗位" />
+        </el-form-item>
+        <el-form-item label="状态" prop="state">
+          <el-select v-model="dialogForm.state" style="width: 140px">
+            <el-option label="在职" :value="1" />
+            <el-option label="离职" :value="2" />
+            <el-option label="试用期" :value="3" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="系统角色" prop="roleList">
+          <el-select v-model="dialogForm.roleList" style="width: 140px" placeholder="请选择用户系统角色">
+            <el-option></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="部门" prop="deptId">
+          <el-cascader 
+            v-model="dialogForm.deptId" 
+            :options="[
+              {
+                _id: 1,
+                deptName: 'ssss',
+                children: [
+                  {
+                    _id: 1,
+                    deptName: 'ssss'}
+                ]
+              }
+            ]" 
+            :props="{
+              checkStrictly: true,
+              value: '_id',
+              label: 'deptName'
+            }" clearable placeholder="请选择部门" style="width: 140px" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button>取消</el-button>
+          <el-button type="primary">确定</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 
 </template>
@@ -58,9 +114,9 @@ export default {
     const { proxy, ctx } = instance;
     // 初始化用户表单对象
     const user = reactive({
-      state: 0
+      state: 3
     });
-    // 初始话用户列表数据
+    // 初始化用户列表数据
     const userList = ref([]);
     // 初始化分页对象
     const pager = reactive({
@@ -107,8 +163,42 @@ export default {
       { 
         label: '最后登录', prop: 'lastLoginTime' },
     ])
-    // 初始话选中列表
+    // 初始化选中列表
     let checkedUserIds = ref([]);
+    // 弹框显示对象
+    let showModal = ref(false);
+    // 新增用户form对象
+    const dialogForm = reactive({
+      state: 3
+    })
+    // 定义表单校验规则
+    const rules = {
+      userName: [
+        { required: true, message:'请输入用户名称', trigger: 'blur'}
+      ],
+      userEmail: [
+        { required: true, message: '请输入用户邮箱', trigger: 'blur' }
+      ],
+      mobile: [
+        {
+          validator(rule, value, callback) {
+            const reg = /^1\d{10}$/
+            if (!value) {
+              callback();
+            } else if (!reg.test(value)) {
+              callback('请输入正确格式的手机号')
+            } else {
+              callback();
+            }
+          }, 
+          trigger: 'blur' 
+        }
+      ],
+      deptId: [
+        { required: true, message: '请输入用户所属部门', trigger: 'blur' },
+        {validator(value){console.log(value)}, trigger: 'blur'}
+      ]
+    };
     // 初始化接口调用
     onMounted(() => {
       getUserList();
@@ -177,18 +267,26 @@ export default {
         console.error(error);
       }
     }
+    // 用户新增
+    const handleCreate = () => {
+      showModal.value = true;
+    }
     return { 
       user, 
       userList, 
       columns, 
       pager,
+      showModal,
+      dialogForm,
+      rules,
       getUserList, 
       resetForm, 
       handleQuery,
       handleCurrentChange,
       handleDel,
       handleSelectionChange,
-      handlePatchDel
+      handlePatchDel,
+      handleCreate
     }
   }
 }
