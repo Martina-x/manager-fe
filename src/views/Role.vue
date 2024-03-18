f2e3dc172a1983b8f301b08c2b14c062f76320b0
Martina <10175022248@qq.com>
2024-03-18 21:58:24 +0800

    feat: 9-2角色操作（创建、编辑、删除）

--- src/views/Role.vue
+++ src/views/Role.vue
@@ -20,9 +20,9 @@
           :formatter="item.formatter" />
         <el-table-column fixed="right" label="操作" width="220">
           <template #default="scope">
-            <el-button size="small">编辑</el-button>
+            <el-button size="small" @click="handleEdit(scope.row)">编辑</el-button>
             <el-button type="primary" size="small">设置权限</el-button>
-            <el-button type="danger" size="small">删除</el-button>
+            <el-button type="danger" size="small" @click="handleDel(scope.row._id)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -31,41 +31,14 @@
           :page-size="pager.pageSize" @current-change="handleCurrentChange" />
       </div>
     </div>
-    <!-- <el-dialog v-model="showModal" title="创建菜单" :close-on-click-modal="false" :close-on-press-escape=false
+    <el-dialog v-model="showModal" title="创建角色" :close-on-click-modal="false" :close-on-press-escape=false
       @close="handleClose">
       <el-form :model="dialogForm" label-width="100px" :rules="rules" ref="dialogFormRef">
-        <el-form-item label="父级菜单" prop="parentId">
-          <el-cascader v-model="dialogForm.parentId" :options="roleList"
-            :props="{ checkStrictly: true, value: '_id', label: 'menuName' }" clearable placeholder="请选择父级菜单"
-            style="margin-right: 15px;" />
-          <span>不选，则直接创建一级菜单</span>
+        <el-form-item label="角色名称" prop="roleName">
+          <el-input v-model="dialogForm.roleName" placeholder="请输入角色名称" />
         </el-form-item>
-        <el-form-item label="菜单类型" prop="menuType">
-          <el-radio-group v-model="dialogForm.menuType">
-            <el-radio :label="1">菜单</el-radio>
-            <el-radio :label="2">按钮</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="菜单名称" prop="menuName">
-          <el-input v-model="dialogForm.menuName" placeholder="请输入菜单名称" />
-        </el-form-item>
-        <el-form-item label="权限标识" prop="menuCode" v-show="dialogForm.menuType == 2">
-          <el-input v-model="dialogForm.menuCode" placeholder="请输入权限标识" />
-        </el-form-item>
-        <el-form-item label="菜单图标" prop="icon" v-show="dialogForm.menuType == 1">
-          <el-input v-model="dialogForm.icon" placeholder="请输入菜单图标" />
-        </el-form-item>
-        <el-form-item label="路由地址" prop="path" v-show="dialogForm.menuType == 1">
-          <el-input v-model="dialogForm.path" placeholder="请输入路由地址" />
-        </el-form-item>
-        <el-form-item label="组件路径" prop="component" v-show="dialogForm.menuType == 1">
-          <el-input v-model="dialogForm.component" placeholder="请输入组件路径" />
-        </el-form-item>
-        <el-form-item label="菜单状态" prop="menuState">
-          <el-radio-group v-model="dialogForm.menuState">
-            <el-radio :label="1">正常</el-radio>
-            <el-radio :label="2">停用</el-radio>
-          </el-radio-group>
+        <el-form-item label="备注" prop="remark">
+          <el-input type="textarea" :rows="2" v-model="dialogForm.remark" placeholder="请输入备注" />
         </el-form-item>
       </el-form>
       <template #footer>
@@ -74,7 +47,7 @@
           <el-button type="primary" @click="handleSubmit">确 定</el-button>
         </div>
       </template>
-    </el-dialog> -->
+    </el-dialog>
   </div>
 
 </template>
@@ -112,6 +85,12 @@ export default {
       pager: {
         pageSize: 10
       },
+      showModal: false,
+      dialogForm: {},
+      rules: {
+        roleName: { required: true, message: '请输入角色名称', trigger: 'blur' }
+      },
+      action: ''
     }
   },
   mounted() {
@@ -132,6 +111,51 @@ export default {
     // 表单重置
     handleReset(form) {
       this.$refs[form].resetFields();
+    },
+
+    // 角色创建
+    handleAdd() {
+      this.showModal = true;
+      this.action = 'create';
+    },
+
+    // 角色编辑
+    handleEdit(row) {
+      this.showModal = true;
+      this.action = 'edit ';
+      this.$nextTick(() => {
+        Object.assign(this.dialogForm, row)
+      })
+    },
+
+    // 角色删除
+    async handleDel(_id) {
+      await this.$api.roleSubmit({ action: 'delete', _id });
+      this.$message.success('删除成功');
+      this.handleClose();
+      this.getRoleList();
+    },
+
+    // 弹框关闭
+    handleClose() {
+      this.showModal = false;
+      this.handleReset('dialogFormRef')
+    },
+
+    // 角色提交
+    handleSubmit() {
+      this.$refs['dialogFormRef'].validate(async (valid) => {
+        if (valid) {
+          let { dialogForm, action } = this;
+          const res = await this.$api.roleSubmit({ action, ...dialogForm });
+        } else {
+          return false
+        }
+        this.$message.success('操作成功');
+        this.handleClose();
+        this.getRoleList();
+      })
+
     }
   }
 }
